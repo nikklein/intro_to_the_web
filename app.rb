e4c857c1fd33f858cf077c6ff6a91a74433da4e8
Thady Condon <thadycondon@hotmail.com>
2016-07-18 17:18:00 +0100

    Refactored the view file and moved the name array to the controller.

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 
 set :session_secret, 'super secret'
 
+
 get '/' do
   "Hello World"
 end
@@ -15,5 +16,6 @@ get '/nextpage' do
 end
 
 get '/cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
